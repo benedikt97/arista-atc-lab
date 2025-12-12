d8cb9d557748e28efd13ac8c220a2ae0dfd6db9d
benedikt97 <heuser@live.de>
2025-12-12 19:20:03 +0100

    stabilized frr

--- clab/startscripts/router1.sh
+++ clab/startscripts/router1.sh
@@ -4,6 +4,12 @@ set -x
 while ! pgrep zebra >/dev/null; do
     sleep 0.1
 done
+while ! pgrep static >/dev/null; do
+    sleep 0.1
+done
+while ! pgrep bgp >/dev/null; do
+    sleep 0.1
+done
 ip tunnel add gre2 mode gre local 138.10.1.1 remote 138.10.1.2
 ip link set gre0 up
 vtysh -f router.cmd
--- clab/startscripts/router2.sh
+++ clab/startscripts/router2.sh
@@ -4,6 +4,12 @@ set -x
 while ! pgrep zebra >/dev/null; do
     sleep 0.1
 done
+while ! pgrep static >/dev/null; do
+    sleep 0.1
+done
+while ! pgrep bgp >/dev/null; do
+    sleep 0.1
+done
 ip tunnel add gre2 mode gre local 138.10.1.2 remote 138.10.1.1
 ip link set gre2 up
 vtysh -f router.cmd
